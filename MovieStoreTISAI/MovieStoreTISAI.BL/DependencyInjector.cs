acb89341581733ad83a22a8599e8c67dd06f5eeb
AtanasTIS4kurs <stu2113891013@uni-plovdiv.bg>
2024-11-17 20:19:07 +0200

    Movie part 2

---
+++ MovieStoreTISAI/MovieStoreTISAI.BL/DependencyInjector.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MovieStoreTISAI.BL.Interfaces;
+using MovieStoreTISAI.BL.Services;
+
+namespace MovieStoreC.BL
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection services)
+        {
+            return services.AddSingleton<IMoviesService, MoviesService>();
+        }
+    }
+}
---
+++ MovieStoreTISAI/MovieStoreTISAI.DL/DependencyInjector.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MovieStoreTISAI.DL.Interfaces;
+using MovieStoreTISAI.DL.Repositories;
+
+namespace MovieStoreC.DL
+{
+
+    public static class DependencyInjection
+    {
+        public static IServiceCollection RegisterRepositories(this IServiceCollection services)
+        {
+            return services.AddSingleton<IMovieRepository, MovieStaticDataRepository>();
+        }
+    }
+}
